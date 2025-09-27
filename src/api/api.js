31229a101af6623bcf780139314bead1e3a18efa
Akarsh Mishra <155228025+akarshmi@users.noreply.github.com>
2025-09-27 12:27:19 +0530

    Changed the  base url to my api api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://51.20.32.249/",
+  baseURL: "https://lyricalcoder-sih.hf.space/",
   // baseURL: "https://lyricalcoder-sih.hf.space",
   headers: {
     "Content-Type": "application/json",
